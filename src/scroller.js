2d0a21a055beb66233df6b2301da6273048b1e03
baterflyrity <baterflyrity@yandex.ru>
2021-01-06 23:20:35 +0300

    Update scroller.js
    See https://github.com/odnari/tabScrollTop2/issues/2#issue-778219140

--- src/scroller.js
+++ src/scroller.js
@@ -1,22 +1,15 @@
 !(function scroller() {
-    let onTop = window.TBST2_ext_onTop;
     let prevX = window.TBST2_ext_prevX;
     let prevY = window.TBST2_ext_prevY;
 
-    if (onTop && (prevX || prevY)) {
-        window.scrollTo(prevX, prevY);
-
-        onTop = false;
+    if (window.scrollX * 2 < document.documentElement.clientWidth && window.scrollY * 2 < document.documentElement.clientHeight) {
+          if (!!prevX || !!prevY) window.scrollTo(prevX, prevY); 
     } else {
         prevX = window.scrollX;
         prevY = window.scrollY;
-
-        window.scrollTo(0, 0);
-
-        onTop = true;
+        window.scrollTo(0, 0);  
     }
-
-    window.TBST2_ext_onTop = onTop;
+    
     window.TBST2_ext_prevX = prevX;
     window.TBST2_ext_prevY = prevY;
 }());
